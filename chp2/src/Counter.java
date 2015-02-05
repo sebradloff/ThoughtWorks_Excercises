e0980757ebb28ac1ae1aae420f7c7797a5a8dfc8
Sebastian Radloff <sradloff23@gmail.com>
2015-02-05 12:43:01 -0600

    completed Counter class and it's main

--- chp2/src/Counter.java
+++ chp2/src/Counter.java
@@ -2,4 +2,26 @@
  * Created by sebass on 2/5/15.
  */
 public class Counter {
+    int count = 0;
+
+    void increment(){
+        count++;
+    }
+
+    void total() {
+        System.out.println("You called the increment method " + count + " times.");
+    }
+
+    public static void main(String []args){
+        Counter c1 = new Counter();
+        int num = 0;
+
+        while (num < 5){
+            c1.increment();
+            num++;
+        }
+
+        c1.total();
+    }
+
 }
