3585376dbb10fb85cacfe2bcb689d8bb249a411e
Sebastian Radloff <sradloff23@gmail.com>
2015-02-07 18:54:11 -0600

    created GuessGame class and constructor

---
+++ chp5/src/GuessGame.java
@@ -0,0 +1,15 @@
+/**
+ * Created by sebass on 2/7/15.
+ */
+public class GuessGame {
+    private int winningNum;
+
+    public GuessGame(){
+        winningNum = (int) (Math.random() * 100) + 1;
+    }
+
+    public static void main(String []args){
+        GuessGame game1 = new GuessGame();
+        
+    }
+}
