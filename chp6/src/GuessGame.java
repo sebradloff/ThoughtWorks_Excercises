b9818c9e816c1246c5bb34c3ea003248514cb0bd
Sebastian Radloff <sradloff23@gmail.com>
2015-02-11 12:44:22 -0600

    copy and pasted over logic from chp5 classes

--- chp6/src/GuessGame.java
+++ chp6/src/GuessGame.java
@@ -2,4 +2,38 @@
  * Created by sebass on 2/11/15.
  */
 public class GuessGame {
+    private int winningNum;
+    private boolean gameOver;
+
+    public GuessGame(){
+        winningNum = (int) (Math.random() * 100) + 1;
+        gameOver = false;
+    }
+
+    public void rateGuess(String guess){
+        int guessNum = Integer.parseInt(guess);
+        if (guessNum > winningNum){
+            System.out.println("Your guess was too high!");
+        } else if (guessNum < winningNum){
+            System.out.println("Your guess was too low!");
+        } else {
+            System.out.println("You guessed correctly!");
+            gameOver = true;
+        }
+    }
+
+    public boolean isGameOver(){
+        return gameOver;
+    }
+
+    public static void main(String []args) {
+        GuessGame game1 = new GuessGame();
+        GuessGameHelper gameHelper = new GuessGameHelper();
+
+        while (game1.isGameOver() == false) {
+            String prompt = "Please enter a number between 1 and 100: ";
+            String guess = gameHelper.getUserInput(prompt);
+            game1.rateGuess(guess);
+        }
+    }
 }
